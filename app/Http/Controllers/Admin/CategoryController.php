6b3bba1922f2f8b0d6fa5e8fa34b64a4faa6a3c1
mahmoud-rafiq <abueslam2898864@gmail.com>
2023-08-02 08:55:28 +0300

    update-2

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\File;
 
 class CategoryController extends Controller
 {
@@ -12,7 +14,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::orderBy('id', 'desc')->paginate(10);
+        return view('admin.categories.index', compact('categories'));
     }
 
     /**
@@ -20,7 +23,8 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::whereNull('parent_id')->get();
+        return view('admin.categories.create', compact('categories'));
     }
 
     /**
@@ -28,7 +32,24 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validate input
+        $request->validate([
+            'name' => 'required',
+            'image' => 'required',
+            'parent_id' => 'nullable||exists:categories,id'
+        ]);
+        //upload the file
+        $new_image = rand() . rand() . time() . $request->file('image')->getClientOriginalName();
+        $request->file('image')->move(public_path('uploads/images'), $new_image);
+
+        //save data to database
+        Category::create([
+            'name' => $request->name,
+            'image' => $new_image,
+            'parent_id' => $request->parent_id
+        ]);
+
+        return redirect()->route('admin.categories.index')->with('msg', 'Category Created')->with('type', 'success');
     }
 
     /**
@@ -44,7 +65,9 @@ class CategoryController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $category = Category::findOrFail($id);
+        $categories = Category::whereNull('parent_id')->where('parent_id', '<>',$category->id)->get();
+        return view('admin.categories.edit', compact('categories', 'category'));
     }
 
     /**
@@ -52,7 +75,29 @@ class CategoryController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+         //validate input
+         $request->validate([
+            'name' => 'required',
+            'image' => 'nullable',
+            'parent_id' => 'nullable||exists:categories,id'
+        ]);
+
+        $category=Category::findorfail($id);
+        $new_image=$category->image;
+        //upload the file
+       if($request->hasFile('image')){
+        $new_image = rand() . rand() . time() . $request->file('image')->getClientOriginalName();
+        $request->file('image')->move(public_path('uploads/images'), $new_image);
+
+       }
+        //save data to database
+        $category->update([
+            'name' => $request->name,
+            'image' => $new_image,
+            'parent_id' => $request->parent_id
+        ]);
+
+        return redirect()->route('admin.categories.index')->with('msg', 'Category Updeted')->with('type', 'info');
     }
 
     /**
@@ -60,6 +105,17 @@ class CategoryController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $category = Category::findOrFail($id);
+
+        //delete image
+        if (file_exists(public_path('uploads/images/' . $category->image))) {
+            File::delete(public_path('uploads/images/' . $category->image));
+        }
+        //set parent id to null
+        Category::where('parent_id', $category->id)->update(['parent_id' => null]);
+        //delete item
+        $category->delete();
+
+        return redirect()->route('admin.categories.index')->with('msg', 'Category deleted')->with('type', 'danger');
     }
 }
---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,60 @@
+@extends('admin.master')
+
+@section('title', 'All Categories | ' . env('APP_NAME'))
+
+@section('styles')
+<style>
+    .table th, .table td{
+        vertical-align: middle;
+    }
+</style>
+@stop
+@section('content')
+    <!-- Page Heading -->
+    <div class="d-flex justify-content-between align-items-center mb-4">
+
+        <h1 class="h3 mb-0 text-gray-800">All Categories</h1>
+        <a class="btn btn-dark" href="{{ route('admin.categories.create') }}">Add New Category</a>
+    </div>
+    @if (session('msg'))
+    <div class="alert alert-{{session('type')}}">{{session('msg')}}</div>
+
+    @endif
+    <table class="table table-hover table-bordered table-striped ">
+        <tr>
+
+            <th>ID</th>
+            <th>Name</th>
+            <th>Image</th>
+            <th>Parent</th>
+            <th>Action</th>
+
+
+        </tr>
+        @foreach ($categories as $category)
+            <tr>
+
+                <td>{{ $category->id }}</td>
+                <td>{{ $category->name }}</td>
+                <td><img width="100" src="{{asset('uploads/images/'.$category->image)}}" alt=""></td>
+                <td>{{ $category->parent->name }}</td>
+                <td>
+                    <a class="btn btn-sm btn-primary" href="{{ route('admin.categories.edit', $category->id) }}">
+                        <i class="fas fa-edit"></i></a>
+                    <form class="d-inline" action="{{ route('admin.categories.destroy', $category->id) }}" method="POST">
+                        @csrf
+                        @method('delete');
+                        <button class="btn btn-sm btn-danger" onclick="return confirm('are you sure?')"><i
+                                class="fas fa-trash"></i> </button>
+                    </form>
+                </td>
+
+
+            </tr>
+        @endforeach
+
+    </table>
+
+    {{ $categories->links() }}
+
+@stop
