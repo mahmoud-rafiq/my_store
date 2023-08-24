4b5a2ac8f6b224e97e6873cde478927bc4791110
mahmoud-rafiq <abueslam2898864@gmail.com>
2023-08-24 23:18:13 +0300

    update

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class SiteController extends Controller
 {
 public function index()
 {
-    return view('site.index');
+    $main_categoris = Category::whereNull('parent_id')->take(11)->get();
+    return view('site.index', compact('main_categoris'));
 }
 }
